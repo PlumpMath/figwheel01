361d358f2d05f4d418d65acffd1187a94cb99fe2
griffio <griffio@users.noreply.github.com>
2016-04-01 17:52:22 +0100

    2d shadow slider

--- src/figwheel01/core.cljs
+++ src/figwheel01/core.cljs
@@ -1,5 +1,8 @@
 (ns ^:figwheel-always figwheel01.core
-  (:require [goog.dom :as dom]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [goog.dom :as dom]
+            [goog.events :as events]
+            [cljs.core.async :refer [put! chan <!]]))
 
 (enable-console-print!)
 
@@ -14,7 +17,11 @@
 (defn on-js-reload []
   (swap! app-state inc))                                    ;; optionally touch app-state to call add-watch[ers]
 
-(reset! app-state @app-state)                               ;; touch appstate
+(defn listens [el type]
+  (let [out (chan)]
+    (events/listen el type
+                   (fn [e] (put! out e)))
+    out))
 
 (defn init-canvas! [h w]
   (let [canvas (dom/getElement "js_canvas")]
@@ -24,11 +31,23 @@
 (defn draw2d! []
   (set! (.-fillStyle ctx2d) "#FA6900")
   (set! (.-shadowOffsetX ctx2d) @app-state)
-  (set! (.-shadowOffsetX ctx2d) @app-state)
-  (set! (.-shadowBlur ctx2d) 4)
+  (set! (.-shadowOffsetY ctx2d) @app-state)
+  (set! (.-shadowBlur ctx2d) 0)
   (set! (.-shadowColor ctx2d) "rgba(204, 204, 204, 0.5)")
   (.fillRect ctx2d 0 0 125 150))
 
-(init-canvas! 600 800)
+(defn animate! []
+  (.clearRect ctx2d (.-shadowOffsetX ctx2d) (.-shadowOffsetY ctx2d) 125 150)
+  (set! (.-shadowOffsetX ctx2d) @app-state)
+  (set! (.-shadowOffsetY ctx2d) @app-state)
+  (.fillRect ctx2d 0 0 125 150))
 
+(let [sliding (listens (dom/getElement "js_slider") "change")]
+  (go (while true
+        (let [value (-> (<! sliding) (.-target) (.-value))]
+          (reset! app-state value)))))
+
+(add-watch app-state :animate #(-> (js/requestAnimationFrame animate!)))
+
+(init-canvas! 600 800)
 (draw2d!)
